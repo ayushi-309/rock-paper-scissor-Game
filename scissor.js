a18ff5bdcfcb515371763684c154670a78df28fb
ayushi-309 <tayushi751@gmail.com>
2025-06-14 21:51:19 +0530

    rock paper scissor game

--- scissor.js
+++ scissor.js
@@ -3,9 +3,17 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+     //rock, paper, scissor
+}
+
 const playGame = (userChoice) => {
     console.log("user choice = ", userChoice);
     //Generate computer choice -> modular
+    const compChoice = genCompChoice;
+    console.log("comp choice = ", compChoice);
 }
 
 choices.forEach((choice) => {
